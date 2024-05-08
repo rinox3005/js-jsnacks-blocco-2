04f0491d10a6373de3862d08933c20afe12ca90d
rinox3005 <peppe.rinox@gmail.com>
2024-05-08 19:11:04 +0200

    [UPD] snack-bonus done

---
+++ snack-bonus/js/snack-bonus.js
@@ -0,0 +1,45 @@
+'use strict';
+
+// array in input
+const arrayA = ['pappa', 'ciotola', 'palla', 'corda', 'guinzaglio', 'snacks'];
+
+console.log('Starting array:', arrayA)
+
+// elemento da inserire in testa
+
+const elementE = 'pettorina';
+
+// assegno ad una variabile length il risultato delle operazioni della funzione contaElementi
+const length = contaElementi(arrayA);
+
+
+// funzione per contare gli elementi di un array
+
+function contaElementi(input) {
+
+    let i = 0;
+
+    while (input[i] !== undefined) {
+        i++;
+    }
+
+    return i;
+}
+
+// funzione che rimuove il primo elemento dell'array
+
+function rimuoviDallaTesta(array1, element) {
+
+    const arrayNew = [];
+    arrayNew.push(element);
+
+    for (let i = 0; i < contaElementi(array1); i++) {
+
+        arrayNew[i + 1] = array1[i];
+    }
+
+    return arrayNew;
+}
+
+// stampo in console l'array risultante
+console.log('Final array:', rimuoviDallaTesta(arrayA, elementE));
