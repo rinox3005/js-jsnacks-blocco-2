139c73c52f0e697b9bd4bbdadeeec7b278765a80
rinox3005 <peppe.rinox@gmail.com>
2024-05-08 16:04:10 +0200

    [UPD] snack-a done

--- snack-a/js/snack-a.js
+++ snack-a/js/snack-a.js
@@ -1,1 +1,27 @@
 'use strict';
+
+//array in input
+
+const inputArray = [1, 22, 31, 4, 55, 64, 37, 81, 23, 2, 17];
+
+// funzione per contare gli elementi presenti nell'array
+
+function contaElementi(input) {
+
+    let i = 0;
+
+    while (input[i] !== undefined) {
+        i++;
+    }
+
+    return i;
+}
+
+// creo una nuova variabile che contenga il risultato della funzione che conta gli elementi 
+
+let arrayLength = contaElementi(inputArray);
+
+// stampo il risultato in console
+
+console.log('The array has', arrayLength, 'elements');
+
